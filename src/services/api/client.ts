f04722d6faf7139c42cf56bce711f43d2da4e0df
MatthewCline-git <matthewcline1028@gmail.com>
2025-03-15 16:53:39 -0400

    Get rid of old print statement

--- src/services/api/client.ts
+++ src/services/api/client.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 import logger from "../../utils/logger";
 
 const apiLogger = logger.component("API");
-const env_var = import.meta.env.VITE_BASE_API_URL;
-console.log("env_var", env_var);
 const BASE_API_URL =
   import.meta.env.VITE_BASE_API_URL || "http://localhost:8000/api/v1";
 const apiClient = axios.create({
